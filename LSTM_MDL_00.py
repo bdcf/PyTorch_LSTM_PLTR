91d61cc68b7ed546907a6d1d69c77fd5d7ffa9bc
bdcf <53155385+bdcf@users.noreply.github.com>
2025-06-21 19:13:54 -0400

    Create LSTM_MDL_00.py
    Set up the data into testing and training for features and tests then loaded those into datasets. Then I put those datasets into data loaders to ready them to be used by the AI for training and testing

---
+++ LSTM_MDL_00.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy as dc
+from sklearn.preprocessing import MinMaxScaler
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+data = pd.read_csv('AMZN.csv')
+# As we are only forecasting the stocks price at close each day we will 
+# only use that data.
+data = data[['Date', 'Close']]
+# Convert the raw Date data to a Pandas datetime object.
+data['Date'] = pd.to_datetime(data['Date'])
+
+def prepLSTMDataFrame(df, n_steps):
+    # Load each row as a dataframe for the LSTM.
+    df = dc(df)
+    df.set_index('Date', inplace=True)
+    # Creates n_steps new columns with previous Close values, shifted back 
+    # 1 to n_steps.
+    for i in range(1, n_steps + 1):
+        df[f'Close(t-{i})'] = df['Close'].shift(i)
+    # If any columns are N/A then we get rid of them.
+    df.dropna(inplace=True)
+    return df
+lookBack = 5
+shiftDf = prepLSTMDataFrame(data, lookBack)
+shiftDfNp = shiftDf.to_numpy()
+# Scales the data between -1 and 1 as preprocessing to normalize data.
+scaler = MinMaxScaler(feature_range=(-1, 1))
+shiftDfNp = scaler.fit_transform(shiftDfNp)
+features = shiftDfNp[:, 1:]
+targets = shiftDfNp[:, 0]
+# Flip the features so 1 is first and lookback is last.
+features = dc(np.flip(features, axis=-1))
+# Uses 95% to train and 5% to test
+split = int(len(features) * 0.95)
+fTrain, fTest, tTrain, tTest = features[:split], features[split:], targets[:split],targets[split:]
+fTrain = torch.tensor(fTrain, dtype=torch.float32).unsqueeze(2)  
+fTest = torch.tensor(fTest, dtype=torch.float32).unsqueeze(2)    
+tTrain = torch.tensor(tTrain, dtype=torch.float32).unsqueeze(1)            
+tTest = torch.tensor(tTest, dtype=torch.float32).unsqueeze(1)
+# print(f'{fTest.shape}{fTrain.shape}{tTest.shape}{tTrain.shape}') to 
+# make sure the unsqueeze added an extra dimension [Testing].
+# We have to build a dataset of our train and test data of tuples for
+# AI training and testing
+class buildDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+trainDataset = buildDataset(fTrain, tTrain)
+testDataset = buildDataset(fTest, tTest)
+# We now will put our dataset into a data loader to normalize batch 
+# sizes for our normalized data.(friends don't let friends use batch 
+# sizes of >32)
+batchSize = 16
+# Set shuffle to true on training data so the AI does not get used 
+# to the training data order
+trainLoader = DataLoader(trainDataset, batch_size=batchSize, shuffle=True) 
+testLoader = DataLoader(testDataset, batch_size=batchSize, shuffle=False)
